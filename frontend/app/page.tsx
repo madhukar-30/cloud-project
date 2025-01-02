104b07611a72dda824f93042b3ff9a3777b08f0c
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-01-02 23:36:52 +0530

    create frontend
    created frontend in nextjs

---
+++ frontend/app/page.tsx
@@ -0,0 +1,149 @@
+'use client';
+
+import Loader from "@/components/Loader";
+import Login from "@/components/Login";
+import Navbar from "@/components/Navbar";
+import { UserContext } from "@/context/userContext";
+import React, { useContext, useEffect } from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  files: z.any().refine((file) => file instanceof FileList && file.length > 0, {
+    message: "Please upload at least one file.",
+  }),
+});
+
+export default function Home() {
+  const { authenticated, loading, setNavTracker, setLoading, userData, user } = useContext(UserContext);
+
+  useEffect(() => {
+    setNavTracker('home');
+  }, [setNavTracker]);
+
+  const email = user;
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      files: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!values.files || values.files.length === 0) {
+      console.error("No files selected");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('cloud', values.files[0]);
+    formData.append('email', email);
+
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:5555/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to upload file: ${response.statusText}`);
+      }
+
+      const result = await response.json();
+      console.log("Upload successful:", result);
+
+      const fileUrl = result.url;
+
+      const responseNew = await fetch('/api/users/update', {
+        method: 'POST',
+        body: JSON.stringify({ email, fileUrl }),
+      });
+
+      if (!responseNew.ok) {
+        throw new Error(`Failed to add url to the array: ${responseNew.statusText}`);
+      }
+
+      console.log(`File URL (${fileUrl}) added to user's files section.`);
+
+      form.reset();
+    } catch (error) {
+      console.error("Error uploading files:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <div className='w-full md:h-screen h-full min-h-screen flex flex-col items-center bg-black p-5 relative'>
+
+        {loading && <Loader />}
+
+        <div className='flex flex-col w-full min-h-full my-auto justify-start rounded-2xl bg-[#E6E4D5] shadow-md'>
+
+          {authenticated ? (
+            <div className='flex flex-col h-full w-full items-center'>
+
+              <Navbar />
+
+              <div className='flex flex-col w-full md:px-28 px-10 h-auto items-center justify-center mt-10'>
+                <div className='md:text-8xl text-4xl font-black uppercase'>Manas Cloud Share</div>
+
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="mt-10">
+                    <FormField
+                      control={form.control}
+                      name="files"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Files</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="file"
+                              multiple
+                              onChange={(e) =>
+                                form.setValue("files", e.target.files)
+                              }
+                            />
+                          </FormControl>
+                          <FormDescription>
+                            Upload one or more files.
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit" className="mt-6">Submit</Button>
+                  </form>
+                </Form>
+
+              </div>
+
+            </div>
+          ) : (
+            <Login />
+          )}
+
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+// so as we're sending the files from the homepage, we don't have to add anything in the backend as after sending the files we'll dynamically update the files url.
---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,65 @@
+import React, { useContext, useState } from 'react';
+
+import Link from 'next/link';
+import { FaGoogle } from 'react-icons/fa';
+import { signIn, signOut } from 'next-auth/react';
+import { UserContext } from '@/context/userContext';
+import Image from 'next/image';
+import { LogInIcon, LogOutIcon, Menu } from 'lucide-react';
+
+const Navbar = () => {
+    const { authenticated, navTracker } = useContext(UserContext);
+    const [mobileNav, setMobileNav] = useState(false);
+
+    return (
+        <div className='w-full h-14 flex items-center justify-between'>
+
+            {/* first half */}
+            <div className='md:w-[25%] w-5/12 h-full flex items-center justify-center bg-black'>
+                <div className='w-full h-full flex items-center justify-center bg-[#E6E4D5] rounded-t-2xl'>
+                    <Link href='/' className='mt-3'>
+                        <Image src={'/assets/manas-cloud.svg'} alt='' width={160} height={40} />
+                    </Link>
+                </div>
+            </div>
+
+            {/* middle one */}
+            <div className='w-[50%] bg-black h-full md:flex hidden items-center justify-center gap-8 rounded-b-2xl text-white uppercase pb-2'>
+                <Link href={'/'} className={navTracker === 'home' ? 'nav--active' : 'nav--links'}>Home</Link>
+                <Link href={'/about'} className={navTracker === 'about' ? 'nav--active' : 'nav--links'}>About</Link>
+                <Link href={'/profile'} className={navTracker === 'profile' ? 'nav--active' : 'nav--links'}>Profile</Link>
+            </div>
+
+            <div className='w-2/12 bg-black h-full md:hidden flex items-center justify-center gap-8 rounded-b-2xl text-white uppercase pb-2 relative'>
+                <Menu className='cursor-pointer' onClick={() => setMobileNav(!mobileNav)} />
+                <div className={`absolute top-16 rounded-2xl shadow p-5 ${mobileNav ? 'flex' : 'hidden'} items-center justify-center z-20 flex-col bg-black w-80 h-80 gap-6`}>
+                    <Link href={'/'} className={navTracker === 'home' ? 'nav--active' : 'nav--links'}>Home</Link>
+                    <Link href={'/about'} className={navTracker === 'about' ? 'nav--active' : 'nav--links'}>About</Link>
+                    <Link href={'/profile'} className={navTracker === 'profile' ? 'nav--active' : 'nav--links'}>Profile</Link>
+                </div>
+            </div>
+
+            {/* second half */}
+            <div className='md:w-[25%] w-5/12 h-full flex items-center justify-center bg-black'>
+                <div className='w-full h-full flex items-center justify-center bg-[#E6E4D5] rounded-t-2xl gap-2'>
+                    <Link href='/' className='mt-3 border-2 border-black font-medium uppercase transition-all hover:bg-black hover:text-white rounded-full px-6 h-8 flex items-center justify-center'>
+                        Reach Us
+                    </Link>
+                    {authenticated ? (
+                        <div className='mt-3 border-2 border-black transition-all hover:bg-black hover:text-white rounded-full h-8 w-8 p-1 md:flex hidden items-center justify-center cursor-pointer' onClick={() => signOut()}>
+                            <LogOutIcon />
+                        </div>
+                    ) : (
+                        <div className='mt-3 border-2 border-black transition-all hover:bg-black hover:text-white rounded-full h-8 w-8 p-1 md:flex hidden items-center justify-center cursor-pointer' onClick={() => signIn()}>
+                            <LogInIcon />
+                        </div>
+                    )}
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Navbar
